e9d538d70a57641b05d5b93330e20ac6323e1e04
YulianSan <knoob0733@gmail.com>
2023-12-06 20:29:39 -0300

    fix(base.block-header): prevent width if menu is closed

--- components/Base/BlockHeader.vue
+++ components/Base/BlockHeader.vue
@@ -5,8 +5,11 @@ interface PropsLinkIcon {
 defineProps<PropsLinkIcon>()
 </script>
 <template>
-  <div class="flex items-center gap-x-2 p-4 hover:bg-zinc-200 dark:hover:bg-zinc-800 rounded-lg">
-    <icon :name="$props.icon" class="text-black text-2xl dark:text-white"/>
-    <slot />
+  <div class="w-full flex items-center gap-x-8 p-4 hover:bg-zinc-200 
+    dark:hover:bg-zinc-800 rounded-lg">
+    <icon :name="$props.icon" class="text-black flex-none w-6 text-2xl dark:text-white"/>
+    <div class="flex-auto break-keep whitespace-nowrap">
+      <slot />
+    </div>
   </div>
 </template>
