092a4f80e2f6c7caf326cdedc06d6e8e5a9ef88b
YulianSan <knoob0733@gmail.com>
2023-12-06 20:30:25 -0300

    feat(header.menu): adding more links and button to toggle menu

--- components/Layout/Header.vue
+++ components/Layout/Header.vue
@@ -1,16 +1,23 @@
 <script setup lang="ts">
 const lightTheme = useTheme()
+const menuIsOpen = useMenu()
 </script>
 <template>
-	<header class="w-full max-w-xs h-screen dark:bg-zinc-950 bg-zinc-100 rounded-2xl p-4 flex flex-col">
-		<div class="mb-12 flex gap-x-4">
-			<div class="bg-purple-800 w-16 h-16 rounded-md"></div>
-			<div class="py-2">
-				<h1 class="font-bold text-xl">
-					Águia Vitrais
-				</h1>
-				<span>Alex Gui. A. Acero</span>
-			</div>
+	<header class="w-full transition-all h-screen dark:bg-zinc-950 py-8 bg-zinc-100 
+		p-4 flex flex-col overflow-x-hidden"
+		:class="[
+			{ 'max-w-xs': menuIsOpen }, 
+			{ 'max-w-[5.5rem]': !menuIsOpen },
+		]">
+		<div class="mb-12 flex gap-x-4 border-b dark:border-zinc-700 py-2">
+			<BaseBlockHeader icon="icon-park-outline:eagle">
+				<div class="py-2">
+					<h1 class="font-bold text-xl">
+						Águia Vitrais
+					</h1>
+					<span>Alex Gui. A. Acero</span>
+				</div>
+			</BaseBlockHeader>
 		</div>
 		<nav>
 			<ul class="flex flex-col gap-y-2">
@@ -44,13 +51,21 @@ const lightTheme = useTheme()
 				</li>
 			</ul>
 		</nav>
-		<div class="mt-auto">
-			<BaseBlockHeader icon="heroicons-solid:sun">
-				<label class="flex justify-between flex-1">
-					Modo Claro
-					<BaseSwitch v-model="lightTheme" />
-				</label>
-			</BaseBlockHeader>
+		<div class="mt-auto border-t py-4 dark:border-zinc-700">
+			<label >
+				<BaseBlockHeader icon="heroicons-solid:sun">
+					<div class="flex w-full justify-between flex-1">
+						<span>Modo Claro</span>
+						<BaseSwitch v-model="lightTheme" />
+					</div>
+				</BaseBlockHeader>
+			</label>
+			<label>
+				<BaseBlockHeader icon="icomoon-free:menu">
+					Menu
+					<input type="checkbox" v-model="menuIsOpen" class="hidden">
+				</BaseBlockHeader>
+			</label>
 		</div>
 	</header>
 </template>
