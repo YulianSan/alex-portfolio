ca7c43cbd57d5f531c280f1dd521387cfd7dcccb
YulianSan <knoob0733@gmail.com>
2023-12-06 20:20:00 -0300

    fix: adding flex 1 in main

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="flex bg-white text-black dark:bg-zinc-900 min-h-screen dark:text-white">
     <LayoutHeader />
-    <slot />
+    <main class="flex-1">
+      <slot />
+    </main>
   </div>
 </template>
