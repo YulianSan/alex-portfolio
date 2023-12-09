4df3fcb3dc7ef0a4f560cdae4fdec68efd4a9bdb
YulianSan <knoob0733@gmail.com>
2023-12-09 17:52:43 -0300

    feat(layout.default): adding padding in top when width is mobile

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex bg-white text-black dark:bg-zinc-900 min-h-screen dark:text-white">
     <LayoutHeader />
-    <main class="flex-1">
+    <main class="flex-1 sm:pt-0 pt-16">
       <slot />
     </main>
   </div>
