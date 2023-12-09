7d310b9b308ee39a14c10a80981bd2c226695a2d
YulianSan <knoob0733@gmail.com>
2023-12-09 18:24:53 -0300

    feat(components.header): adding base and client header

--- components/Layout/Header.vue
+++ components/Layout/Header.vue
@@ -1,71 +1,24 @@
 <script setup lang="ts">
-const lightTheme = useTheme()
 const menuIsOpen = useMenu()
 </script>
+
 <template>
+	<LayoutMenuHamburger />
+	<slot name="before-header" />
 	<header class="w-full transition-all h-screen dark:bg-zinc-950 py-8 bg-zinc-100 
-		p-4 flex flex-col overflow-x-hidden"
+		p-4 flex flex-col overflow-x-hidden border-r border-zinc-300 dark:border-zinc-700
+		sm:translate-x-0 -translate-x-full fixed sm:relative max-w-xs select-none"
 		:class="[
-			{ 'max-w-xs': menuIsOpen }, 
-			{ 'max-w-[5.5rem]': !menuIsOpen },
+			{ 'sm:max-w-xs': menuIsOpen }, 
+			{ 'translate-x-0': menuIsOpen }, 
+			{ 'sm:max-w-[5.5rem]': !menuIsOpen },
 		]">
 		<div class="mb-12 flex gap-x-4 border-b dark:border-zinc-700 py-2">
-			<BaseBlockHeader icon="icon-park-outline:eagle">
-				<div class="py-2">
-					<h1 class="font-bold text-xl">
-						Águia Vitrais
-					</h1>
-					<span>Alex Gui. A. Acero</span>
-				</div>
-			</BaseBlockHeader>
+			<slot name="top-header" />
 		</div>
-		<nav>
-			<ul class="flex flex-col gap-y-2">
-				<li>
-					<BaseLink href="/">
-						<BaseBlockHeader icon="uil:home">
-							Home
-						</BaseBlockHeader>
-					</BaseLink>
-				</li>
-				<li>
-					<BaseLink href="/about">
-						<BaseBlockHeader icon="material-symbols:info">
-							Sobre nós
-						</BaseBlockHeader>
-					</BaseLink>
-				</li>
-				<li>
-					<BaseLink href="/works">
-						<BaseBlockHeader icon="material-symbols:info">
-							Obras de Arte
-						</BaseBlockHeader>
-					</BaseLink>
-				</li>
-				<li>
-					<BaseLink href="/contact">
-						<BaseBlockHeader icon="material-symbols:info">
-							Contato
-						</BaseBlockHeader>
-					</BaseLink>
-				</li>
-			</ul>
-		</nav>
+		<slot />
 		<div class="mt-auto border-t py-4 dark:border-zinc-700">
-			<label >
-				<BaseBlockHeader icon="heroicons-solid:sun">
-					<div class="flex w-full justify-between flex-1">
-						<span>Modo Claro</span>
-						<BaseSwitch v-model="lightTheme" />
-					</div>
-				</BaseBlockHeader>
-			</label>
-			<label>
-				<BaseBlockHeader icon="icomoon-free:menu">
-					Menu
-					<input type="checkbox" v-model="menuIsOpen" class="hidden">
-				</BaseBlockHeader>
-			</label>
+			<slot name="bottom-header" />
 		</div>
 	</header>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex bg-white text-black dark:bg-zinc-900 min-h-screen dark:text-white">
-    <LayoutHeader />
+    <LayoutHeaderClient />
     <main class="flex-1 sm:pt-0 pt-16">
       <slot />
     </main>
