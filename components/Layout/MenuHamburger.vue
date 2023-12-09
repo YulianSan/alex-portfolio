e6c11faff99f160e59030acb4aaf0e45f5ad2952
YulianSan <knoob0733@gmail.com>
2023-12-09 18:01:53 -0300

    feat(components.menu): adding component menu hamburger

---
+++ components/Layout/MenuHamburger.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const menuIsOpen = useMenu()
+</script>
+<template>
+	<label class="z-50 sm:hidden w-14 h-14 flex items-center justify-center
+		fixed p-2 rounded-md right-6 top-6">
+		<Icon name="icomoon-free:menu" class="text-4xl" />
+		<input type="checkbox" v-model="menuIsOpen" class="hidden">
+	</label>
+</template>
