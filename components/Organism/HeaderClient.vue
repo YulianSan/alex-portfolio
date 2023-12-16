b95cb1136bb27afff22f0ed247ec94e61183ec9b
YulianSan <knoob0733@gmail.com>
2023-12-16 20:34:38 -0300

    fix: improving atomic

--- components/Organism/HeaderClient.vue
+++ components/Organism/HeaderClient.vue
@@ -4,46 +4,46 @@ const menuIsOpen = useMenu()
 </script>
 
 <template>
-	<MoleculeHeader>
+	<OrganismHeader>
 		<template #top-header>
-			<AtomBlockHeader icon="icon-park-outline:eagle">
+			<MoleculeBlockHeader icon="icon-park-outline:eagle">
 				<div class="py-2">
 					<h1 class="font-bold text-xl">
 						Águia Vitrais
 					</h1>
 					<span>Alex Gui. A. Acero</span>
 				</div>
-			</AtomBlockHeader>
+			</MoleculeBlockHeader>
 		</template>
 		<template #default>
 			<nav>
 				<ul class="flex flex-col gap-y-2">
 					<li>
 						<AtomLink href="/">
-							<AtomBlockHeader icon="uil:home">
+							<MoleculeBlockHeader icon="uil:home">
 								Home
-							</AtomBlockHeader>
+							</MoleculeBlockHeader>
 						</AtomLink>
 					</li>
 					<li>
 						<AtomLink href="/about">
-							<AtomBlockHeader icon="ri:information-line">
+							<MoleculeBlockHeader icon="ri:information-line">
 								Sobre nós
-							</AtomBlockHeader>
+							</MoleculeBlockHeader>
 						</AtomLink>
 					</li>
 					<li>
 						<AtomLink href="/works">
-							<AtomBlockHeader icon="ri:pencil-ruler-2-line">
+							<MoleculeBlockHeader icon="ri:pencil-ruler-2-line">
 								Obras de Arte
-							</AtomBlockHeader>
+							</MoleculeBlockHeader>
 						</AtomLink>
 					</li>
 					<li>
 						<AtomLink href="/contact">
-							<AtomBlockHeader icon="ri:contacts-line">
+							<MoleculeBlockHeader icon="ri:contacts-line">
 								Contato
-							</AtomBlockHeader>
+							</MoleculeBlockHeader>
 						</AtomLink>
 					</li>
 				</ul>
@@ -51,19 +51,19 @@ const menuIsOpen = useMenu()
 		</template>
 		<template #bottom-header>
 			<label >
-				<AtomBlockHeader icon="heroicons-solid:sun">
+				<MoleculeBlockHeader icon="heroicons-solid:sun">
 					<div class="flex w-full justify-between flex-1">
 						<span>Modo Claro</span>
 						<AtomSwitch v-model="lightTheme" />
 					</div>
-				</AtomBlockHeader>
+				</MoleculeBlockHeader>
 			</label>
 			<label class="sm:block hidden">
-				<AtomBlockHeader icon="icomoon-free:menu">
+				<MoleculeBlockHeader icon="icomoon-free:menu">
 					Menu
 					<input type="checkbox" v-model="menuIsOpen" class="hidden">
-				</AtomBlockHeader>
+				</MoleculeBlockHeader>
 			</label>
 		</template>
-	</MoleculeHeader>
+	</OrganismHeader>
 </template>
