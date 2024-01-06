f02ee05128cdc044ea3825c0586597901cbf1eb8
YulianSan <knoob0733@gmail.com>
2024-01-05 21:50:31 -0300

    feat(atom.icon): adding component

---
+++ components/Atom/Icon.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { twMerge } from 'tailwind-merge'
+import { computed } from 'vue';
+interface PropsIcon {
+  icon: string
+  class?: string
+}
+
+const props = withDefaults(defineProps<PropsIcon>(), {
+  class: ""
+})
+
+const classIcon = computed(
+  () => 
+    twMerge(props.class, "text-black flex-none w-6 text-2xl dark:text-white"),
+)
+</script>
+<template>
+  <Icon
+    :name="$props.icon"
+    :class="classIcon"
+  />
+</template>
