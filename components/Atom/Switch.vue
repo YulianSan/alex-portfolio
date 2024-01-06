7dd803bffb59d7d9fa9eb6095935d85b64fec1d4
YulianSan <knoob0733@gmail.com>
2024-01-05 21:49:49 -0300

    fix(atom.switch): wrapping with label

--- components/Atom/Switch.vue
+++ components/Atom/Switch.vue
@@ -12,16 +12,20 @@ const modelValueComputed = computed({
   get: () => props.modelValue,
   set: (value) => emit('update:modelValue', !!value)
 })
+
 </script>
 <template>
-  <input class="hidden" type="checkbox" v-model="modelValueComputed" />
-  <div class="w-12 h-6 bg-zinc-300 dark:bg-black rounded-full relative cursor-pointer">
-    <div class="w-5 h-5 bg-zinc-700 dark:bg-zinc-800 rounded-full absolute top-1/2 -translate-y-1/2 transition-[right] ease-in-out"
-      :class="[
-        { 'right-1': props.modelValue }, 
-        { 'right-[calc(100%-1.5rem)]': !props.modelValue }
-      ]">
+  <label>
+    <input class="hidden" role="switch" type="checkbox" v-model="modelValueComputed" />
+    <div class="w-12 h-6 bg-zinc-300 dark:bg-black rounded-full relative cursor-pointer">
+      <div
+        class="w-5 h-5 bg-zinc-700 dark:bg-zinc-800 rounded-full absolute top-1/2 -translate-y-1/2 transition-[right] ease-in-out"
+        :class="[
+                  { 'right-1': props.modelValue }, 
+                  { 'right-[calc(100%-1.5rem)]': !props.modelValue }
+                ]"
+      >
+      </div>
     </div>
-  </div>
+  </label>
 </template>
-<!--  -->
