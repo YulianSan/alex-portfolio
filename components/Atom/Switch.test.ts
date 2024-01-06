ae0c14c707fcf97089caf9ef1fc04128a4a9c290
YulianSan <knoob0733@gmail.com>
2024-01-05 21:55:17 -0300

    test(atom.switch): adding basic test

---
+++ components/Atom/Switch.test.ts
@@ -0,0 +1,34 @@
+import { fireEvent, render, screen } from '@testing-library/vue'
+import { it, describe, expect } from 'vitest'
+// @ts-ignore
+import Switch from './Switch.vue'
+
+describe('Switch', () => {
+  it('should renders', () => {
+    render(Switch)
+
+    expect(screen.getByRole('switch')).toBeInTheDocument()
+  })
+
+  it('should toggle value', async () => {
+    const { emitted } = render(Switch, {
+      props: {
+        modelValue: false,
+      },
+    })
+
+    const checkbox = screen.getByRole('switch')
+
+    expect(checkbox).not.toBeChecked()
+
+    await fireEvent.click(checkbox)
+
+    expect(emitted()['update:modelValue'][0]).toEqual([true]);
+    expect(checkbox).toBeChecked()
+
+    await fireEvent.click(checkbox)
+
+    expect(emitted()['update:modelValue'][1]).toEqual([false]);
+    expect(checkbox).not.toBeChecked()
+  })
+})
