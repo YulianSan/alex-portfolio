89df218012d98022a8bf7639e587938b499f6f64
YulianSan <knoob0733@gmail.com>
2024-01-11 14:27:35 -0300

    test(molecule.hamburger): adding test

--- components/Molecule/Hamburger.test.ts
+++ components/Molecule/Hamburger.test.ts
@@ -1,5 +1,5 @@
 import { vi, describe, it, expect } from 'vitest'
-import { render, screen } from '@testing-library/vue'
+import { fireEvent, render, screen } from '@testing-library/vue'
 // @ts-ignore
 import Hamburger from './Hamburger.vue'
 
@@ -7,8 +7,28 @@ vi.mock('../../composables/useMenu')
 
 describe('Hamburger', () => {
 	it('should render', () => {
-		render(Hamburger)
+		render(Hamburger, {
+			props: {
+				modelValue: false
+			},
+		})
 
 		expect(screen.getByRole('button')).toBeInTheDocument()
+	})
+
+	it('should open menu', () => {
+		const { emitted } = render(Hamburger, {
+			props: {
+				modelValue: false
+			},
+		})
+
+		const button = screen.getByRole('button')
+		expect(emitted('update:modelValue')?.length).toBeUndefined()
+		fireEvent.click(button)
+
+		expect(emitted()).toHaveProperty('update:modelValue')
+		expect(emitted('update:modelValue')?.[0]).toEqual([true])
+		expect(emitted('update:modelValue')?.length).toBe(1)
 	})
 })
