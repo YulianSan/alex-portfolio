6a3d01aac57a6c86a04320862f4a69f05ae51d69
YulianSan <knoob0733@gmail.com>
2024-01-11 14:26:57 -0300

    feat(molecule.hamburger): using modelValue instead couple a value and using new feature defineModel

--- components/Molecule/Hamburger.vue
+++ components/Molecule/Hamburger.vue
@@ -1,10 +1,10 @@
 <script setup lang="ts">
-const menuIsOpen = useMenu()
+const modelValue = defineModel<boolean>()
 </script>
 <template>
 	<label role="button" class="z-50 sm:hidden w-14 h-14 flex items-center justify-center
 		fixed p-2 rounded-md right-6 top-6">
 		<AtomIcon icon="icomoon-free:menu" class="text-4xl" />
-		<input type="checkbox" v-model="menuIsOpen" class="hidden">
+		<input type="checkbox" v-model="modelValue" class="hidden">
 	</label>
 </template>
