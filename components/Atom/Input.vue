3cb149aed0e9da03f41ae673b9e1c7e27ddbe799
YulianSan <knoob0733@gmail.com>
2024-01-16 20:47:34 -0300

    feat(atom.input): basic input

---
+++ components/Atom/Input.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+const modelValue = defineModel<string>();
+</script>
+<template>
+  <label>
+    <slot />
+    <div class="flex gap-x-2 w-full py-2 border-[1px] border-zinc-300 rounded-md"
+      :class="{
+       'px-4': !$slots.icon,
+       'px-2': $slots.icon,
+      }">
+      <slot name="icon" />
+      <input
+        class="w-full outline-none text-sm bg-transparent"
+        type="text"
+        v-model="modelValue"
+      />
+    </div>
+  </label>
+</template>
