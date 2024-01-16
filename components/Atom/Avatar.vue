4dfd26fb9dd0f977437604edb2420b0c613b93d6
YulianSan <knoob0733@gmail.com>
2024-01-16 20:49:51 -0300

    feat(atom.avatar): basic component

---
+++ components/Atom/Avatar.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+type AvatarFormat = 'circle' | 'square' | 'rounded'
+
+interface Props {
+  src: string
+  format?: AvatarFormat
+}
+
+withDefaults(defineProps<Props>(), {
+  format: 'square',
+})
+</script>
+<template>
+  <NuxtImg 
+    fit="cover" 
+    :src="$props.src" 
+    class="w-24 h-24 object-cover"
+    :class="{
+      'rounded-full': $props.format === 'circle',
+      'rounded-md': $props.format === 'rounded',
+      'rounded-none': $props.format === 'square',
+    }"
+    width="96" 
+    height="96" />
+</template>
