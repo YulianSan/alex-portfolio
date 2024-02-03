a3a338e1afbad67839ef6258c6dabf1efbf6473b
YulianSan <knoob0733@gmail.com>
2024-02-03 16:10:50 -0300

    feat: example commit prev

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,40 @@
+<script setup lang="ts">
+import { type Ref, ref } from 'vue'
+import { type ModelValue } from '@/components/Atom/DropImage.vue'
+
+const files: Ref<ModelValue[]> = ref([])
+
+const removeImage = (index: number) => {
+	files.value.splice(index, 1)
+}
+
+watch(files, () => {
+	console.log(files.value)
+})
+</script>
 <template>
-	<AtomCard>Home page</AtomCard>
+	<AtomDropImage v-model="files"> 
+		<template #default="{ isHover }">
+			<div
+				class="w-64 h-64"
+				:class="[
+					{ 'bg-red-300': isHover },
+					{ 'bg-blue-300': !isHover },
+				]" >
+				<AtomIcon v-if="isHover" icon="uil:dropbox" />
+				<AtomIcon v-else icon="uil:home" />
+			</div>
+		</template>
+	</AtomDropImage>
+	<div class="max-w-xs">
+		<MoleculeImageDrop
+			@remove="removeImage"
+      v-for="(file, i) in files"
+			:src="file.src"
+			:name="file.name"
+			:size="file.size"
+      :key="file.src + file.name"
+			:index="i"
+    />
+	</div>
 </template>
